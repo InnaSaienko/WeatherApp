f7d988312f17309f7fea2fd6a48823f8721935ce
Inna Saienko <inna.saienko.nl@gmail.com>
2024-08-27 17:29:32 +0200

    Uninstalled i18n-countries

--- src/components/WeatherToday/WeatherToday.js
+++ src/components/WeatherToday/WeatherToday.js
@@ -1,23 +1,18 @@
 import React from "react";
-import countries from "i18n-iso-countries";
-import enLocale from "i18n-iso-countries/langs/en.json";
 import "./WeatherToday.scss";
-
-countries.registerLocale(enLocale);
 
 class WeatherToday extends React.Component {
   render() {
-    console.log("props for today:")
     const tempC = Math.round(this.props.todayWeather.main.temp - 273, 15);
     const weather = this.props.todayWeather.weather[0].main;
     const iconId = this.props.todayWeather.weather[0].icon;
-    const countryCode = this.props.city.country;
-    const country = countries.getName(countryCode, "en");
+    const countryName = this.props.countryName;
+    const city = this.props.cityName;
 
     return (
       <div className="weather-today">
         <h3 className="title-3">
-          {this.props.city.name}, {country}
+          {city}, {countryName}
         </h3>
         <div className="weather-info">
           <div className="weather-icon">
