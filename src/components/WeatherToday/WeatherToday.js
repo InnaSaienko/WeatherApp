c594c1ae4fead727c769c085687783b83e72986a
Inna Saienko <inna.saienko.nl@gmail.com>
2024-08-29 09:47:42 +0200

    Removed City and CountryName to WeatherInfo.js

--- src/components/WeatherToday/WeatherToday.js
+++ src/components/WeatherToday/WeatherToday.js
@@ -6,34 +6,32 @@ class WeatherToday extends React.Component {
     const tempC = Math.round(this.props.todayWeather.main.temp - 273, 15);
     const weather = this.props.todayWeather.weather[0].main;
     const iconId = this.props.todayWeather.weather[0].icon;
-    const countryName = this.props.countryName;
-    const city = this.props.cityName;
+    const speed = this.props.todayWeather.wind.speed;
+    const precip = this.props.todayWeather.pop;
+    const pressure = this.props.todayWeather.main.pressure;
+    // const countryName = this.props.countryName;
+    // const city = this.props.cityName;
 
     return (
       <div className="weather-today">
-        <h3 className="title-3">
-          {city}, {countryName}
-        </h3>
-        <div className="weather-info">
-          <div className="weather-icon">
-            <img
-              src={require(`../../assets/images/${iconId}.svg`)}
-              alt="Weather Icon"
-            />
-            <p>{weather}</p>
-          </div>
-          <h2 className="title-2">{tempC}°C</h2>
-          <div className="weather-more">
-            <p className="text">
-              Wind: <span>{this.props.todayWeather.wind.speed} kmph</span>
-            </p>
-            <p className="text">
-              Precip: <span>{this.props.todayWeather.pop} %</span>
-            </p>
-            <p className="text">
-              Pressure: <span>{this.props.todayWeather.main.pressure}mb</span>
-            </p>
-          </div>
+        <div className="weather-icon">
+          <img
+            src={require(`../../assets/images/${iconId}.svg`)}
+            alt="Weather Icon"
+          />
+          <p>{weather}</p>
+        </div>
+        <div className="weather-title"><h2 className="title-2">{tempC}°C</h2></div>
+        <div className="weather-more">
+          <p className="text">
+            Wind: <span>{speed} kmph</span>
+          </p>
+          <p className="text">
+            Precip: <span>{precip} %</span>
+          </p>
+          <p className="text">
+            Pressure: <span>{pressure}mb</span>
+          </p>
         </div>
       </div>
     );
