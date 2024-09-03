23584bcaa2018928f50804642f8f1b90f54ca1b6
Inna Saienko <inna.saienko.nl@gmail.com>
2024-09-03 10:25:12 +0200

    Change formatting

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -1,48 +1,48 @@
-function getLatLon (place) {
-    let geoLocation = {
-      lat: null,
-      lng: null,
-    };
-
-    if ("geometry" in place && "location" in place.geometry) {
-      geoLocation.lat = place.geometry.location.lat();
-      geoLocation.lng = place.geometry.location.lng();
-    }
-    return geoLocation;
+function getLatLon(place) {
+  let geoLocation = {
+    lat: null,
+    lng: null,
   };
 
-  function getAddress (place) {
-    let address = {
-      city: null,
-      countryCode: null,
-      countryName: null,
-    };
+  if ("geometry" in place && "location" in place.geometry) {
+    geoLocation.lat = place.geometry.location.lat();
+    geoLocation.lng = place.geometry.location.lng();
+  }
+  return geoLocation;
+}
 
-    if (!place) {
-      return address;
-    }
-    const addressComponents = place.address_components;
+function getAddress(place) {
+  let address = {
+    city: null,
+    countryCode: null,
+    countryName: null,
+  };
 
-    if (addressComponents) {
-      for (const component of addressComponents) {
-        if (!component.types) {
-          continue;
-        }
-        if (component.types.includes("locality")) {
-          address.city = component.long_name;
-        }
-        if (component.types.includes("country")) {
-          address.countryCode = component.short_name;
-          address.countryName = component.long_name;
-        }
-        if (address.city !== null && address.countryCode !== null) {
-          break;
-        }
-      }
-    } else {
-      address.city = place.name;
-    }
+  if (!place) {
     return address;
-  };
+  }
+  const addressComponents = place.address_components;
 
-  export { getLatLon, getAddress }
+  if (addressComponents) {
+    for (const component of addressComponents) {
+      if (!component.types) {
+        continue;
+      }
+      if (component.types.includes("locality")) {
+        address.city = component.long_name;
+      }
+      if (component.types.includes("country")) {
+        address.countryCode = component.short_name;
+        address.countryName = component.long_name;
+      }
+      if (address.city !== null && address.countryCode !== null) {
+        break;
+      }
+    }
+  } else {
+    address.city = place.name;
+  }
+  return address;
+}
+
+export { getLatLon, getAddress };
